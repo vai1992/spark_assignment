9ee270b93344c079e0646543ce39b15f0dd83c68
Vaibhav Agrawal <vaibhav.agrawal@elastic.run>
2020-10-21 11:32:53 +0530

    Complete Commit of the project Final

--- project/total_score.py
+++ project/total_score.py
@@ -11,7 +11,7 @@ spark = SparkSession._create_shell_session()
 
 
 #Reading Input from the Input File Provided
-df = spark.read.option("header",True).csv('../input/IPL_SCHEDULE_2008_2020.csv')
+df = spark.read.option("header",True).csv('input/IPL_SCHEDULE_2008_2020.csv')
 
 #Filtering out all eliminators, qualifiers and final matches from the data
 df = df.filter(lower(col('Match_Info')).contains('match'))
@@ -26,7 +26,7 @@ df = df.fillna('null')
 df = df.filter(col('Match_Result') != 'null')
 
 #Reading Team Mappings with their city(keywords) from a created file. This gives the name of latest Team names corresponding to the city/word
-team_mapping = spark.read.option("header",True).csv('../extra_input/Team Mapping.csv')
+team_mapping = spark.read.option("header",True).csv('extra_input/Team Mapping.csv')
 
 
 #Extracting Team 1 and Team 2 names from the Match_Team field using " vs " as splitter
@@ -76,4 +76,4 @@ final_df = final_df.withColumn("Tied",F.when(final_df["Tied"], 1).otherwise(0))
 final_df = final_df.groupBy("IPL_year", "Team").agg(count("*").alias("Matches Played") , sum("Won").alias("Matches_Won") , sum("Lost").alias("Matches_Lost") , sum("Tied").alias("Matches_Tied") , sum("Points").alias("Overall_Points")  ).orderBy('IPL_year',desc('Overall_Points'),desc("Matches_Won"))
 
 #Writing, Storing the end result in an output file
-final_df.repartition(1).write.mode('overwrite').format('com.databricks.spark.csv').save('../output',header = 'true')
+final_df.repartition(1).write.mode('overwrite').format('com.databricks.spark.csv').save('output',header = 'true')
